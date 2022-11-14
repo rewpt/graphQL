7d503d5750e55808a13a0e0d3784c8a5d4e9a71c
rewpt <zpermack@gmail.com>
2022-11-13 22:48:36 -0500

    connect to mongodb database

--- server/app.js
+++ server/app.js
@@ -1,7 +1,26 @@
+require('dotenv').config();
 const express = require('express');
-const graphqlHTTP = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const userPw = process.env.CONNECTION_STRING;
+const uri = `mongodb+srv://rewpt:${userPw}@graphqlproject.jzybpgq.mongodb.net/?retryWrites=true&w=majority`;
 
 const app = express();
+
+// connect to mongo-db-atlas db
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
+
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
 const port = 4000;
 
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,13 +1,60 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLList,
+} = graphql;
 
+//dummy data
+const books = [
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+  { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
+  { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+  { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
+];
+
+const authors = [
+  { name: 'Patrick Rothfuss', age: 44, id: '1' },
+  { name: 'Brandon Sanderson', age: 42, id: '2' },
+  { name: 'Terry Pratchett', age: 66, id: '3' },
+];
+
+//Fields are wrapped in a function because they reference types that
+//may be defined later in the file. This way the functions are only run after
+//all of the types get defined
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        return authors.find((author) => author.id === parent.authorId);
+      },
+    },
+  }),
+});
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return books.filter((book) => book.authorId === parent.id);
+      },
+    },
   }),
 });
 
@@ -16,10 +63,38 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        //code to get data from db/other source
+        //code to get data from db/other source goes here
+        //this uses lodash
+        return _.find(books, { id: args.id });
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        //this uses es6
+        return authors.find((author) => author.id == args.id);
+      },
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        //can just return the whole thing and graph ql will
+        //take care of the fields
+        return books;
+      },
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args) {
+        return authors;
       },
     },
   },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
