2c797e0bcc7d82f18762aeb657620a7228a55c9c
rewpt <zpermack@gmail.com>
2022-11-30 17:30:12 -0500

    initial frontend setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import BookList from './components/BookList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="main">
+      <h1>My reading list</h1>
+      <BookList />
     </div>
   );
 }
